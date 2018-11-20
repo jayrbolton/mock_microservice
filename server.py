766a202947aec45f1b88a6b1901f7fb04b748574
Jay R Bolton <jayrbolton@gmail.com>
2018-11-20 08:49:50 -0800

    Get things working

--- server.py
+++ server.py
@@ -2,12 +2,14 @@ import os
 import sys
 import json
 import jsonschema
+import traceback
 import flask
 from jsonschema.exceptions import ValidationError
 
+# Load the endpoints data, the schema, and validate the structure
+
 with open('endpoints_schema.json') as fd:
     endpoints_schema = json.load(fd)
-
 
 if not os.path.exists('/config/endpoints.json'):
     sys.stderr.write('File not found: /config/endpoints.json\n')
@@ -18,12 +20,13 @@ with open('/config/endpoints.json') as fd:
 try:
     jsonschema.validate(endpoints, endpoints_schema)
 except ValidationError as err:
-    sys.stderr.write('Validation Error on endpoints.json:\n')
+    sys.stderr.write('JSON Schema validation Error on endpoints.json:\n')
     sys.stderr.write(str(err) + '\n')
     sys.exit(1)
 
 sys.stdout.write('Loaded %s mock endpoints\n' % len(endpoints))
 
+# Start the Flask app
 app = flask.Flask(__name__)
 methods = ['GET', 'POST', 'PUT', 'DELETE']
 
@@ -31,37 +34,45 @@ methods = ['GET', 'POST', 'PUT', 'DELETE']
 @app.route('/', defaults={'path': ''}, methods=methods)
 @app.route('/<path:path>', methods=methods)
 def handle_request(path):
+    """
+    Catch-all: handle any request against the endpoints.json data.
+    """
     path = '/' + path
-    req_body = flask.request.get_data()
-    try:
-        req_json = json.loads(req_body)
-    except Exception:
-        req_json = None
+    req_body = json.loads(flask.request.get_data() or '{}')
     method = flask.request.method
-    print(req_json)
-    print(req_body)
-    print(endpoints)
-    found_endpoint = None
+    # Find the first endpoint that matches path, method, headers, and body
     for endpoint in endpoints:
-        if method not in endpoint['methods']:
-            continue
-        if endpoint['path'] != path:
-            continue
-        if 'headers' not in endpoint:
-            found_endpoint = endpoint
-            break
-        headers = dict(flask.request.headers)
-        headers_match = True
-        for (key, val) in endpoint['headers'].items():
-            if key not in headers or val != headers[key]:
-                headers_match = False
-                break
-        if headers_match:
-            found_endpoint = endpoint
-    if found_endpoint:
-        return (
-            found_endpoint['response']['body'],
-            found_endpoint['response']['status']
-        )
-    else:
-        return ('"Unknown endpoint."', 500)
+        method_ok = method in endpoint.get('methods', ['GET'])
+        path_ok = endpoint['path'] == path
+        ep_body = endpoint.get('body')
+        body_ok = ep_body is None or ep_body == req_body
+        headers_ok = match_headers(endpoint)
+        if method_ok and path_ok and body_ok and headers_ok:
+            sys.stdout.write('Matched endpoint %s %s\n' % (method, path))
+            resp = endpoint['response']
+            resp_body = flask.jsonify(resp.get('body', ''))
+            return (resp_body, resp['status'])
+    raise Exception('Unable to match endpoint: %s %s' % (method, path))
+
+
+@app.errorhandler(Exception)
+def any_exception(err):
+    """Catch any error with a JSON response."""
+    class_name = err.__class__.__name__
+    print(traceback.format_exc())
+    resp = {'error': str(err), 'class': class_name}
+    return (flask.jsonify(resp), 500)
+
+
+def match_headers(endpoint):
+    """
+    Either check that there are no headers to match, or match that all headers
+    in the endpoint are present and equal in the request.
+    """
+    if 'headers' not in endpoint:
+        return True
+    headers = dict(flask.request.headers)
+    for (key, val) in endpoint['headers'].items():
+        if val != headers.get(key):
+            return False
+    return True
