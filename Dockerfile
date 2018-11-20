8a17a7b712fc1fda6c644a69792a3074d4ea3bc0
Jay R Bolton <jayrbolton@gmail.com>
2018-11-19 22:34:51 -0800

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
 FROM python:3.7-alpine
 
+# Install pip dependencies
 COPY requirements.txt /server/requirements.txt
 WORKDIR /server
-
-# Install dependencies
-RUN apk --update add make
 RUN apk --update add --virtual build-dependencies python-dev build-base && \
     pip install --upgrade pip && \
     pip install --upgrade --no-cache-dir -r requirements.txt && \
     apk del build-dependencies
 
-# Run the app
+# Run the server
 COPY . /server
-CMD ["python", "server.py"]
+ENV PYTHONUNBUFFERED=1
+ENV FLASK_ENV=development
+ENV FLASK_APP=server.py
+CMD ["flask", "run", "--host=0.0.0.0"]
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import jsonschema
+import flask
 from jsonschema.exceptions import ValidationError
 
 with open('endpoints_schema.json') as fd:
@@ -22,3 +23,45 @@ except ValidationError as err:
     sys.exit(1)
 
 sys.stdout.write('Loaded %s mock endpoints\n' % len(endpoints))
+
+app = flask.Flask(__name__)
+methods = ['GET', 'POST', 'PUT', 'DELETE']
+
+
+@app.route('/', defaults={'path': ''}, methods=methods)
+@app.route('/<path:path>', methods=methods)
+def handle_request(path):
+    path = '/' + path
+    req_body = flask.request.get_data()
+    try:
+        req_json = json.loads(req_body)
+    except Exception:
+        req_json = None
+    method = flask.request.method
+    print(req_json)
+    print(req_body)
+    print(endpoints)
+    found_endpoint = None
+    for endpoint in endpoints:
+        if method not in endpoint['methods']:
+            continue
+        if endpoint['path'] != path:
+            continue
+        if 'headers' not in endpoint:
+            found_endpoint = endpoint
+            break
+        headers = dict(flask.request.headers)
+        headers_match = True
+        for (key, val) in endpoint['headers'].items():
+            if key not in headers or val != headers[key]:
+                headers_match = False
+                break
+        if headers_match:
+            found_endpoint = endpoint
+    if found_endpoint:
+        return (
+            found_endpoint['response']['body'],
+            found_endpoint['response']['status']
+        )
+    else:
+        return ('"Unknown endpoint."', 500)
